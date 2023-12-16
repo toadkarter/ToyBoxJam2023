c332533daf28eeeab99033a52f176d9c8fdce6ca
toadkarter <vrakhmanin1@gmail.com>
2023-12-16 15:35:52 +0000

    Added droppable block

---
+++ Scenes/Blocks/Droppable/droppable_block.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+
+@export var seconds_before_falling = 1.0
+@onready var hitbox: Area2D = $Hitbox
+
+
+func _ready() -> void:
+	hitbox.connect("body_entered", _on_hitbox_triggered)
+
+
+func _on_hitbox_triggered(body: Node) -> void:
+	if body.is_in_group("Player"):
+		await get_tree().create_timer(seconds_before_falling).timeout
+		gravity_scale = 1.0
+
+
+
