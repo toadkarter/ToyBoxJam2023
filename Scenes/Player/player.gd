00a96fc9290cf67b31af06cd5d73db5c532fdfaa
toadkarter <vrakhmanin1@gmail.com>
2023-12-16 15:06:15 +0000

    Refactoring animation code into separate methods for readability

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -18,19 +18,8 @@ func _physics_process(delta: float) -> void:
 
 	_handle_move_input()
 
-	if velocity.x > 0 and animations.flip_h:
-		animations.flip_h = false
-	elif velocity.x < 0 and !animations.flip_h:
-		animations.flip_h = true
-
-	
-	if !is_on_floor():
-		animations.play("jump")
-	elif velocity.x == 0:
-		animations.play("default")
-	else:
-		animations.play("run")
-
+	_handle_sprite_flip()
+	_handle_animations()
 
 	move_and_slide()
 
@@ -41,3 +30,19 @@ func _handle_move_input() -> void:
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+
+
+func _handle_sprite_flip() -> void:
+	if velocity.x > 0 and animations.flip_h:
+		animations.flip_h = false
+	elif velocity.x < 0 and !animations.flip_h:
+		animations.flip_h = true
+
+
+func _handle_animations() -> void:
+	if !is_on_floor():
+		animations.play("jump")
+	elif velocity.x == 0:
+		animations.play("default")
+	else:
+		animations.play("run")
