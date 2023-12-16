5bb3902dfa05c7598b316491750592ba909ffebb
toadkarter <vrakhmanin1@gmail.com>
2023-12-16 22:28:20 +0000

    Working on bat movement

--- Scenes/Enemies/Bat/bat.gd
+++ Scenes/Enemies/Bat/bat.gd
@@ -1,9 +1,17 @@
 extends StaticBody2D
 
+@export var speed: float = 40.0
+@export var distance: float = 200.0
 
 @onready var animations: AnimatedSprite2D = $AnimatedSprite2D
 
+var forward: bool = true
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	animations.play("default")
+
+
+func _process(delta: float) -> void:
+	distance -= delta
+	print(distance)
+	position.x += speed * delta
