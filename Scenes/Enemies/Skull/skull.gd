89f61300715c832be04bc1db95eb96e4592591e4
toadkarter <vrakhmanin1@gmail.com>
2023-12-16 17:56:18 +0000

    Allowing delay before starting emission loop

--- Scenes/Enemies/Skull/skull.gd
+++ Scenes/Enemies/Skull/skull.gd
@@ -6,6 +6,7 @@ extends StaticBody2D
 @export var num_projectiles_per_emission: int = 3
 @export var seconds_between_projectile: float = 0.4
 @export var seconds_between_emission: float = 3
+@export var seconds_before_starting_emissions: float = 0.0
 
 @onready var animations: AnimatedSprite2D = $AnimatedSprite2D
 @onready var projectile_spawn_location: Node2D = $ProjectileSpawnLocation
@@ -15,8 +16,9 @@ var num_projectiles_emitted = 0
 
 func _ready() -> void:
 	animations.animation_finished.connect(_on_animation_finished)
+	await get_tree().create_timer(seconds_before_starting_emissions).timeout
+
 	_emit_projectiles()
-	pass
 
 
 func _emit_projectiles() -> void:
@@ -35,5 +37,5 @@ func _spawn_projectile() -> void:
 	var projectile = projectile_scene.instantiate()
 	add_child(projectile)
 	projectile.position = projectile_spawn_location.position
-	projectile.push(20)
+	projectile.push(projectile_speed)
 
