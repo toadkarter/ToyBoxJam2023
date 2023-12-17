f3305117883861d3715f07afddcd6c9505a02bb3
toadkarter <vrakhmanin1@gmail.com>
2023-12-17 19:50:28 +0000

    Added intro with flashing press start symbol

---
+++ Scenes/Game/Intro/Intro.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+@onready var hud: CanvasLayer = $HUD
+@onready var animations: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+	animations.play("press_start_fade_in_and_out")
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("jump"):
+		animations.stop()
+		print("We are starting the intro")
