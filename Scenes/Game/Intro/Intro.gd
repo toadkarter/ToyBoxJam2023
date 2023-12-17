c5e2f47610c931d80e978f4fb99eaa79e67dcec1
toadkarter <vrakhmanin1@gmail.com>
2023-12-17 21:17:02 +0000

    Hooked up intro to the main node with a lead in into the main game

--- Scenes/Game/Intro/Intro.gd
+++ Scenes/Game/Intro/Intro.gd
@@ -21,9 +21,9 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if Input.is_anything_pressed():
+	if Input.is_action_just_pressed("jump"):
 		if scroll_has_started:
-			intro_finished.emit()
+			_finish_intro()
 		else:
 			_start_intro()
 
@@ -36,3 +36,10 @@ func _start_intro() -> void:
 	sky_animations.play("sky_darken")
 	audio_player.play()
 	scroll_has_started = true
+
+
+func _finish_intro() -> void:
+	var seconds_to_wait: float = hud_animations.get_animation("fade_out").length
+	hud_animations.play("fade_out")
+	await get_tree().create_timer(seconds_to_wait).timeout
+	intro_finished.emit()
--- Scenes/Game/Main/main.gd
+++ Scenes/Game/Main/main.gd
@@ -2,9 +2,20 @@ extends Node2D
 
 
 @export var level_scene: PackedScene
+@export var intro_scene: PackedScene
+
+var intro: Node2D
+var level: Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var level = level_scene.instantiate()
+	intro = intro_scene.instantiate()
+	add_child(intro)
+
+	intro.connect("intro_finished", _on_intro_finished)
+
+
+func _on_intro_finished() -> void:
+	intro.queue_free()
+	level = level_scene.instantiate()
 	add_child(level)
