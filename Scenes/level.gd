97ba101ef703e9b595731a15275e2357ce8bf34d
toadkarter <vrakhmanin1@gmail.com>
2023-12-17 00:28:09 +0000

    Dynamically adding player to checkpoint node

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -3,13 +3,18 @@ extends TileMap
 
 @export var scroll_level: bool = true
 @export var scroll_speed: float = 5
+@export var player_scene: PackedScene
 
 @onready var camera: Camera2D = $Camera
 @onready var firewall: Node2D = $Firewall
-@onready var player: CharacterBody2D = $Player # TODO: Access this dynamically
+@onready var checkpoint: Node2D = $Checkpoint
+# @onready var player: CharacterBody2D = $Player # TODO: Access this dynamically
 
 
 func _ready() -> void:
+	var player = load(player_scene.resource_path).instantiate()
+	add_child(player)
+	player.position = checkpoint.position
 	player.connect("on_death", _on_player_death)
 
 
@@ -22,4 +27,5 @@ func _process(delta: float) -> void:
 
 
 func _on_player_death() -> void:
+	print("Hello")
 	scroll_level = false
