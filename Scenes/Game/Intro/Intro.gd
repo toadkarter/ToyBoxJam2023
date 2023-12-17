bbcee7387d4901394fc46d2b9a8d4cc40f4746bd
toadkarter <vrakhmanin1@gmail.com>
2023-12-17 20:54:44 +0000

    Added sky darken animation

--- Scenes/Game/Intro/Intro.gd
+++ Scenes/Game/Intro/Intro.gd
@@ -1,14 +1,36 @@
 extends Node2D
 
 
+signal intro_finished
+
+
+@export var scroll_speed: float = 50.0
+
 @onready var hud: CanvasLayer = $HUD
-@onready var animations: AnimationPlayer = $AnimationPlayer
+@onready var hud_animations: AnimationPlayer = $HUDAnimationPlayer
+@onready var sky_animations: AnimationPlayer = $SkyAnimationPlayer
+@onready var tower: Node2D = $Tower
+
+
+var scroll_has_started: bool = false
 
 
 func _ready() -> void:
-	animations.play("press_start_fade_in_and_out")
+	pass
 
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("jump"):
-		animations.stop()
+	if Input.is_anything_pressed():
+		if scroll_has_started:
+			intro_finished.emit()
+		else:
+			_start_intro()
+
+	if scroll_has_started:
+		tower.position.y -= scroll_speed * delta
+
+
+func _start_intro() -> void:
+	hud_animations.play_backwards()
+	sky_animations.play("sky_darken")
+	scroll_has_started = true
