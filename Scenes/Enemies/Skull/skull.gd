df7ae1b2b71460946425bf13ac435959fbb64f64
toadkarter <vrakhmanin1@gmail.com>
2023-12-18 09:51:58 +0000

    Hacking in a way to reset skull projectiles on death

--- Scenes/Enemies/Skull/skull.gd
+++ Scenes/Enemies/Skull/skull.gd
@@ -14,20 +14,18 @@ extends StaticBody2D
 
 func _ready() -> void:
 	animations.animation_finished.connect(_on_animation_finished)
-	await get_tree().create_timer(seconds_before_starting_emissions).timeout
-
-	_emit_projectiles()
+	_init_projectiles()
 
 
 func _emit_projectiles() -> void:
 	animations.play("attack")
 	for i in range(num_projectiles_per_emission):
 		_spawn_projectile()
-		await get_tree().create_timer(seconds_between_projectile).timeout
+		await _run_timer(seconds_between_projectile)
 
 
 func _on_animation_finished() -> void:
-	await get_tree().create_timer(seconds_between_emission).timeout
+	await _run_timer(seconds_between_emission)
 	_emit_projectiles()
 
 
@@ -37,3 +35,28 @@ func _spawn_projectile() -> void:
 	projectile.position = projectile_spawn_location.position
 	projectile.push(projectile_speed)
 
+
+func _init_projectiles() -> void:
+	await _run_timer(seconds_before_starting_emissions)
+	_emit_projectiles()
+
+
+func reset_state() -> void:
+	animations.stop()
+	for node in get_children():
+		var timer: Timer = node as Timer
+		if !is_instance_valid(timer) or timer.is_queued_for_deletion():
+			continue
+		timer.paused = true
+
+	_init_projectiles()
+
+
+func _run_timer(seconds: float) -> void:
+	var timer: Timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = seconds
+	timer.one_shot = true
+	timer.start()
+	await timer.timeout
+	timer.queue_free()
