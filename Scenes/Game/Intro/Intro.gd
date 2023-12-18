2d6b75567d96e1e429432e019fae8da4b9731709
toadkarter <vrakhmanin1@gmail.com>
2023-12-18 00:22:38 +0000

    Adding extra pause after title fades out and before story starts

--- Scenes/Game/Intro/Intro.gd
+++ Scenes/Game/Intro/Intro.gd
@@ -7,6 +7,7 @@ signal intro_finished
 @export var scroll_speed: float = 50.0
 @export var seconds_for_text_on_screen: float = 5.0
 @export var seconds_between_text: float = 1.0
+@export var seconds_before_showing_text: float = 1.0
 @export_multiline var intro_text: Array[String]
 
 @onready var hud: CanvasLayer = $HUD
@@ -44,7 +45,7 @@ func _start_intro() -> void:
 	scroll_has_started = true
 
 	var title_fade_length: float = hud_animations.get_animation("title_fade").length
-	await get_tree().create_timer(title_fade_length).timeout
+	await get_tree().create_timer(title_fade_length + seconds_before_showing_text).timeout
 
 	title_label.modulate.a = 0
 	subtitle_label.modulate.a = 0
